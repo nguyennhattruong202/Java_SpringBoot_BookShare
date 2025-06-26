42464ffe246923500fcaadad01026caf903f7423
nguyennhattruong202 <nhattruongnguyen202@gmail.com>
2025-06-26 11:35:49 +0700

    build(project): init bookshare project

---
+++ bookshare/src/main/java/vn/bookshare/BookshareApplication.java
@@ -0,0 +1,13 @@
+package vn.bookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshareApplication.class, args);
+	}
+
+}
